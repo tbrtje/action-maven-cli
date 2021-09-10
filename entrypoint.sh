c3f1b78933f204eee5e4338f17ab82ff868cc879
Thies Johann Brötje <kontakt+dev@thiesbroetje.de>
2021-09-10 14:19:51 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ set -e
 echo "==========Starting Maven Commands=========="
 bash -c "if [-z "$GS_CONFIG"]
 then
-  sh -c "mvn $*"
+  sh -c 'mvn $*'
 
 else
-  sh -c "echo $GS_CONFIG > /settings.xml"
-  sh -c "mvn $ -gs /settings.xml*"
+  sh -c 'echo $GS_CONFIG > /settings.xml'
+  sh -c 'mvn $ -gs /settings.xml*'
 fi"
 
 
