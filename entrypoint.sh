556fac17336e1c0f5c1f80455128300734dc5848
Thies Johann Brötje <kontakt+dev@thiesbroetje.de>
2021-09-10 15:01:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,8 @@
 set -e
 
 echo "==========Starting Maven Commands=========="
-bash -c "echo $OWNER $USERNAME $TOKEN"
-bash -c "sed -i 's/OWNER/$OWNER/g' /settings.xml"
-bash -c "sed -i 's/USERNAME/$USERNAME/g' /settings.xml"
-bash -c "sed -i 's/TOKEN/$TOKEN/g' /settings.xml"
-bash -c "mvn $* -gs /settings.xml"
+sh -c "echo $OWNER $USERNAME $TOKEN"
+sh -c "mvn $* -gs /settings.xml"
 
 
 echo "==========Finished Maven Commands=========="
